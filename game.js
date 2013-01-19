0d5033d7b883b2321a1132848a2187853c98ab1b
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2013-01-19 17:40:59 +0100

    Main scene initialized

--- game.js
+++ game.js
@@ -1,4 +1,20 @@
+
 window.onload = function () {
+
+    // Initialisation de Crafty
     Crafty.init(400, 400);
-    //Crafty.canvas.init();
+    Crafty.canvas.init();
+    
+    // Création de la scene principale
+    Crafty.scene("main", function() {
+        // Ajout de la map en image de fond
+        Crafty.e("2D, Canvas, Image").image("assets/map.png");
+    });
+    
+    // Chargement des assets
+    Crafty.load(["assets/map.png"], function () {
+        // Déclenchement de la scène principale
+        Crafty.scene("main");
+    });
+    
 };
